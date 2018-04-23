af0b922b3716e113ce7860321d78c205ad1c1bc5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/InitNewRowWhenEditing/Window1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Editors;
+
+namespace InitNewRowWhenEditing {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+
+            BindingList<TestData> list = new BindingList<TestData>() { AllowNew = true };
+            for (int i = 0; i < 0; i++)
+                list.Add(new TestData() { Text = "Row" + i, Number = i });
+            grid.ItemsSource = list;
+        }
+
+        private void TableView_InitNewRow(object sender, InitNewRowEventArgs e) {
+            grid.SetCellValue(GridControl.NewItemRowHandle, grid.Columns["Text"], "New Row");
+            grid.SetCellValue(GridControl.NewItemRowHandle, grid.Columns["Number"], 999997);
+        }
+
+        bool isLocked;
+        private void TableView_FocusedRowHandleChanged(object sender, FocusedRowHandleChangedEventArgs e) {
+            if (!isLocked && view.FocusedRowHandle == GridControl.NewItemRowHandle) {
+                isLocked = true;
+                view.AddNewRow();
+                isLocked = false;
+            }
+        }
+    }
+
+    public class TestData {
+        public string Text { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ VB/InitNewRowWhenEditing/Window1.xaml.vb
@@ -0,0 +1,57 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports System.ComponentModel
+Imports DevExpress.Xpf.Grid
+Imports DevExpress.Xpf.Editors
+
+Namespace InitNewRowWhenEditing
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Partial Public Class Window1
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+
+            Dim list As New BindingList(Of TestData)() With {.AllowNew = True}
+            For i As Integer = 0 To -1
+                list.Add(New TestData() With { _
+                    .Text = "Row" & i, _
+                    .Number = i _
+                })
+            Next i
+            grid.ItemsSource = list
+        End Sub
+
+        Private Sub TableView_InitNewRow(ByVal sender As Object, ByVal e As InitNewRowEventArgs)
+            grid.SetCellValue(GridControl.NewItemRowHandle, grid.Columns("Text"), "New Row")
+            grid.SetCellValue(GridControl.NewItemRowHandle, grid.Columns("Number"), 999997)
+        End Sub
+
+        Private isLocked As Boolean
+        Private Sub TableView_FocusedRowHandleChanged(ByVal sender As Object, ByVal e As FocusedRowHandleChangedEventArgs)
+            If Not isLocked AndAlso view.FocusedRowHandle = GridControl.NewItemRowHandle Then
+                isLocked = True
+                view.AddNewRow()
+                isLocked = False
+            End If
+        End Sub
+    End Class
+
+    Public Class TestData
+        Public Property Text() As String
+        Public Property Number() As Integer
+    End Class
+End Namespace
