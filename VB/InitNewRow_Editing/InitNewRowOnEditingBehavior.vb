54819a9e0c9962cda1674becf13f5c64782c42fc
DevExpressExampleBot <fakeEmail>
2021-08-12 18:45:43 +0300

    Source auto update [skip ci]

---
+++ VB/InitNewRow_Editing/InitNewRowOnEditingBehavior.vb
@@ -0,0 +1,35 @@
+﻿Imports DevExpress.Mvvm.UI.Interactivity
+Imports DevExpress.Xpf.Grid
+Imports System
+Imports System.Linq
+
+Namespace InitNewRow_Editing
+	Public Class InitNewRowOnEditingBehavior
+		Inherits Behavior(Of TableView)
+
+		Protected Overrides Sub OnAttached()
+			MyBase.OnAttached()
+			AddHandler AssociatedObject.FocusedRowHandleChanged, AddressOf OnFocusedRowHandleChanged
+			AddHandler AssociatedObject.InitNewRow, AddressOf OnInitNewRow
+		End Sub
+		Protected Overrides Sub OnDetaching()
+			AddHandler AssociatedObject.FocusedRowHandleChanged, AddressOf OnFocusedRowHandleChanged
+			AddHandler AssociatedObject.InitNewRow, AddressOf OnInitNewRow
+			MyBase.OnDetaching()
+		End Sub
+
+		Private isLocked As Boolean
+		Private Sub OnFocusedRowHandleChanged(ByVal sender As Object, ByVal e As FocusedRowHandleChangedEventArgs)
+			If Not isLocked AndAlso AssociatedObject.FocusedRowHandle = GridControl.NewItemRowHandle Then
+				isLocked = True
+				AssociatedObject.AddNewRow()
+				isLocked = False
+			End If
+		End Sub
+
+		Private Sub OnInitNewRow(ByVal sender As Object, ByVal e As InitNewRowEventArgs)
+			AssociatedObject.Grid.SetCellValue(GridControl.NewItemRowHandle, AssociatedObject.Grid.Columns("Text"), "New Row")
+			AssociatedObject.Grid.SetCellValue(GridControl.NewItemRowHandle, AssociatedObject.Grid.Columns("Number"), 999997)
+		End Sub
+	End Class
+End Namespace
