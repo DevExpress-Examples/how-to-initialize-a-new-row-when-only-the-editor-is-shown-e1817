9272ae00131352e8c1ea40311fa31fd453bf9b56
volkov.anton <anton.volkov@devexpress.com>
2021-08-12 17:32:31 +0300

    Support MVVM pattern

---
+++ CS/InitNewRow_Editing/InitNewRowOnEditingBehavior.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.Mvvm.UI.Interactivity;
+using DevExpress.Xpf.Grid;
+using System;
+using System.Linq;
+
+namespace InitNewRow_Editing {
+    public class InitNewRowOnEditingBehavior : Behavior<TableView> {
+        protected override void OnAttached() {
+            base.OnAttached();
+            AssociatedObject.FocusedRowHandleChanged += OnFocusedRowHandleChanged;
+            AssociatedObject.InitNewRow += OnInitNewRow;
+        }
+        protected override void OnDetaching() {
+            AssociatedObject.FocusedRowHandleChanged += OnFocusedRowHandleChanged;
+            AssociatedObject.InitNewRow += OnInitNewRow;
+            base.OnDetaching();
+        }
+
+        bool isLocked;
+        void OnFocusedRowHandleChanged(object sender, FocusedRowHandleChangedEventArgs e) {
+            if(!isLocked && AssociatedObject.FocusedRowHandle == GridControl.NewItemRowHandle) {
+                isLocked = true;
+                AssociatedObject.AddNewRow();
+                isLocked = false;
+            }
+        }
+
+        void OnInitNewRow(object sender, InitNewRowEventArgs e) {
+            AssociatedObject.Grid.SetCellValue(GridControl.NewItemRowHandle, AssociatedObject.Grid.Columns["Text"], "New Row");
+            AssociatedObject.Grid.SetCellValue(GridControl.NewItemRowHandle, AssociatedObject.Grid.Columns["Number"], 999997);
+        }
+    }
+}
---
+++ CS/InitNewRow_Editing/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.Xpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace InitNewRow_Editing {
+    public class DataItem {
+        public string Text { get; set; }
+        public int Number { get; set; }
+    }
+    public class MainViewModel : ViewModelBase {
+        public BindingList<DataItem> Items { get; }
+        public MainViewModel() {
+            Items = new BindingList<DataItem>(GetItems().ToList()) { AllowNew = true };
+        }
+        static IEnumerable<DataItem> GetItems() {
+            return Enumerable.Range(0, 10).Select(i => new DataItem() { Text = "Row" + i, Number = i });
+        }
+    }
+}
