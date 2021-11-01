ab1cdb352e4d927001b9d23913447aa809ebd818
DevExpressExampleBot <fakeEmail>
2021-11-01 23:29:35 +0300

    autogenerate VB

--- VB/InitNewRow_Editing/InitNewRowOnEditingBehavior.vb
+++ VB/InitNewRow_Editing/InitNewRowOnEditingBehavior.vb
@@ -1,35 +1,36 @@
-﻿Imports DevExpress.Mvvm.UI.Interactivity
+Imports DevExpress.Mvvm.UI.Interactivity
 Imports DevExpress.Xpf.Grid
-Imports System
-Imports System.Linq
 
 Namespace InitNewRow_Editing
-	Public Class InitNewRowOnEditingBehavior
-		Inherits Behavior(Of TableView)
 
-		Protected Overrides Sub OnAttached()
-			MyBase.OnAttached()
-			AddHandler AssociatedObject.FocusedRowHandleChanged, AddressOf OnFocusedRowHandleChanged
-			AddHandler AssociatedObject.InitNewRow, AddressOf OnInitNewRow
-		End Sub
-		Protected Overrides Sub OnDetaching()
-			AddHandler AssociatedObject.FocusedRowHandleChanged, AddressOf OnFocusedRowHandleChanged
-			AddHandler AssociatedObject.InitNewRow, AddressOf OnInitNewRow
-			MyBase.OnDetaching()
-		End Sub
+    Public Class InitNewRowOnEditingBehavior
+        Inherits Behavior(Of TableView)
 
-		Private isLocked As Boolean
-		Private Sub OnFocusedRowHandleChanged(ByVal sender As Object, ByVal e As FocusedRowHandleChangedEventArgs)
-			If Not isLocked AndAlso AssociatedObject.FocusedRowHandle = GridControl.NewItemRowHandle Then
-				isLocked = True
-				AssociatedObject.AddNewRow()
-				isLocked = False
-			End If
-		End Sub
+        Protected Overrides Sub OnAttached()
+            MyBase.OnAttached()
+            AssociatedObject.FocusedRowHandleChanged += AddressOf OnFocusedRowHandleChanged
+            AssociatedObject.InitNewRow += AddressOf OnInitNewRow
+        End Sub
 
-		Private Sub OnInitNewRow(ByVal sender As Object, ByVal e As InitNewRowEventArgs)
-			AssociatedObject.Grid.SetCellValue(GridControl.NewItemRowHandle, AssociatedObject.Grid.Columns("Text"), "New Row")
-			AssociatedObject.Grid.SetCellValue(GridControl.NewItemRowHandle, AssociatedObject.Grid.Columns("Number"), 999997)
-		End Sub
-	End Class
+        Protected Overrides Sub OnDetaching()
+            AssociatedObject.FocusedRowHandleChanged -= AddressOf OnFocusedRowHandleChanged
+            AssociatedObject.InitNewRow -= AddressOf OnInitNewRow
+            MyBase.OnDetaching()
+        End Sub
+
+        Private isLocked As Boolean
+
+        Private Sub OnFocusedRowHandleChanged(ByVal sender As Object, ByVal e As FocusedRowHandleChangedEventArgs)
+            If Not isLocked AndAlso AssociatedObject.FocusedRowHandle Is GridControl.NewItemRowHandle Then
+                isLocked = True
+                AssociatedObject.AddNewRow()
+                isLocked = False
+            End If
+        End Sub
+
+        Private Sub OnInitNewRow(ByVal sender As Object, ByVal e As InitNewRowEventArgs)
+            AssociatedObject.Grid.SetCellValue(GridControl.NewItemRowHandle, AssociatedObject.Grid.Columns("Text"), "New Row")
+            AssociatedObject.Grid.SetCellValue(GridControl.NewItemRowHandle, AssociatedObject.Grid.Columns("Number"), 999997)
+        End Sub
+    End Class
 End Namespace
--- VB/InitNewRow_Editing/MainViewModel.vb
+++ VB/InitNewRow_Editing/MainViewModel.vb
@@ -1,28 +1,30 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.DataAnnotations
 Imports DevExpress.Mvvm.Xpf
-Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Linq
 
 Namespace InitNewRow_Editing
-	Public Class DataItem
-		Public Property Text() As String
-		Public Property Number() As Integer
-	End Class
-	Public Class MainViewModel
-		Inherits ViewModelBase
 
-		Public ReadOnly Property Items() As BindingList(Of DataItem)
-		Public Sub New()
-			Items = New BindingList(Of DataItem)(GetItems().ToList()) With {.AllowNew = True}
-		End Sub
-		Private Shared Function GetItems() As IEnumerable(Of DataItem)
-			Return Enumerable.Range(0, 10).Select(Function(i) New DataItem() With {
-				.Text = "Row" & i,
-				.Number = i
-			})
-		End Function
-	End Class
+    Public Class DataItem
+
+        Public Property Text As String
+
+        Public Property Number As Integer
+    End Class
+
+    Public Class MainViewModel
+        Inherits ViewModelBase
+
+        Public ReadOnly Property Items As BindingList(Of DataItem)
+
+        Public Sub New()
+            Items = New BindingList(Of DataItem)(GetItems().ToList()) With {.AllowNew = True}
+        End Sub
+
+        Private Shared Function GetItems() As IEnumerable(Of DataItem)
+            Return Enumerable.Range(0, 10).[Select](Function(i) New DataItem() With {.Text = "Row" & i, .Number = i})
+        End Function
+    End Class
 End Namespace
