c829e022211f185a394afc4557c6f4bd2524f3d6
DevExpressExampleBot <scmaintenance@devexpress.com>
2025-07-25 10:07:27 +0300

    Change example status to out of maintenance

--- CS/InitNewRow_Editing/MainWindow.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace InitNewRow_Editing {
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
-    }
-}
