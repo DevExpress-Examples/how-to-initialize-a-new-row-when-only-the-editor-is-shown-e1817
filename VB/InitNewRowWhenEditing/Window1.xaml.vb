f37adf3d40c4750daf239494cc4a0fa97b7bf23e
DevExpressExampleBot <fakeEmail>
2025-07-25 10:56:28 +0400

    Source auto update [skip ci]

--- VB/InitNewRowWhenEditing/Window1.xaml.vb
+++ VB/InitNewRowWhenEditing/Window1.xaml.vb
@@ -1,57 +1,46 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports System.ComponentModel
 Imports DevExpress.Xpf.Grid
-Imports DevExpress.Xpf.Editors
 
 Namespace InitNewRowWhenEditing
+
     ''' <summary>
     ''' Interaction logic for Window1.xaml
     ''' </summary>
-    Partial Public Class Window1
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
+            Dim list As BindingList(Of TestData) = New BindingList(Of TestData)() With {.AllowNew = True}
+            For i As Integer = 0 To 0 - 1
+                list.Add(New TestData() With {.Text = "Row" & i, .Number = i})
+            Next
 
-            Dim list As New BindingList(Of TestData)() With {.AllowNew = True}
-            For i As Integer = 0 To -1
-                list.Add(New TestData() With { _
-                    .Text = "Row" & i, _
-                    .Number = i _
-                })
-            Next i
-            grid.ItemsSource = list
+            Me.grid.ItemsSource = list
         End Sub
 
         Private Sub TableView_InitNewRow(ByVal sender As Object, ByVal e As InitNewRowEventArgs)
-            grid.SetCellValue(GridControl.NewItemRowHandle, grid.Columns("Text"), "New Row")
-            grid.SetCellValue(GridControl.NewItemRowHandle, grid.Columns("Number"), 999997)
+            Me.grid.SetCellValue(DataControlBase.NewItemRowHandle, Me.grid.Columns("Text"), "New Row")
+            Me.grid.SetCellValue(DataControlBase.NewItemRowHandle, Me.grid.Columns("Number"), 999997)
         End Sub
 
         Private isLocked As Boolean
+
         Private Sub TableView_FocusedRowHandleChanged(ByVal sender As Object, ByVal e As FocusedRowHandleChangedEventArgs)
-            If Not isLocked AndAlso view.FocusedRowHandle = GridControl.NewItemRowHandle Then
+            If Not isLocked AndAlso Me.view.FocusedRowHandle = DataControlBase.NewItemRowHandle Then
                 isLocked = True
-                view.AddNewRow()
+                Me.view.AddNewRow()
                 isLocked = False
             End If
         End Sub
     End Class
 
     Public Class TestData
-        Public Property Text() As String
-        Public Property Number() As Integer
+
+        Public Property Text As String
+
+        Public Property Number As Integer
     End Class
 End Namespace
